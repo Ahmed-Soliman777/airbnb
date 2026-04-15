1a5a87f571d7ab901fb6de30c54f582248309afb
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-04-15 23:51:09 +0200

    Create Navbar component

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,121 @@
+'use client'
+
+import Image from 'next/image'
+import Logo from './Logo'
+import { LuMenu, LuSearch } from 'react-icons/lu'
+import { useEffect, useRef, useState } from 'react'
+import { useAuthModal } from '@/store/useAuthModalStore'
+
+const Navbar = () => {
+
+  const { openRegister, openLogin } = useAuthModal()
+
+  const [openDropDownMenu, setOpenDropDownMenu] = useState<boolean>(false)
+
+  const menuRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    // close dropdown menu when user clicks outside of it
+    function handleMouseClickEvent(e: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node))
+        setOpenDropDownMenu(false)
+    }
+
+    document.addEventListener('mousedown', handleMouseClickEvent)
+
+    return () => document.removeEventListener('mousedown', handleMouseClickEvent)
+  }, [])
+
+  return (
+    <nav className='fixed top-0 z-50 w-full h-18 lg:h-24 bg-white border-b border-gray-200'>
+      <div className="flex items-center justify-between h-full mx-auto w-[95%] md:w-[90%]">
+
+        {/* airbnb logo */}
+        <Logo />
+
+        {/* navbar navgation */}
+        <div className="flex items-center gap-3 px-4 py-2 shadow-md border border-gray-200 rounded-full cursor-pointer">
+          <span className='text-sm font-medium text-gray-700 flex items-center gap-2'>
+            <Image
+              src={'/home.png'}
+              alt='home-icon'
+              width={25}
+              height={25}
+            />
+            <span className='hidden lg:block'>Anywhere</span>
+          </span>
+
+          {/* vertical border */}
+          <span className='h-6 w-px bg-gray-300 hidden lg:block' />
+
+          <span className="hidden md:block text-sm text-gray-300">
+            Add guests
+          </span>
+
+          <div className="w-8 h-8 text-white rounded-full bg-primary grid place-items-center">
+            <LuSearch size={16} />
+          </div>
+        </div>
+
+        {/* right navbar */}
+        <div
+          className="flex items-center gap-4 relative"
+          ref={menuRef}
+        >
+          <button className="hidden md:block text-sm font-medium px-4 py-2 rounded-full bg-gray-50 cursor-pointer hover:bg-gray-100">
+            Airbnb your home
+          </button>
+
+          <div className="flex items-center gap-2 border border-gray-300 rounded-full px-2 py-1 hover:shadow-md transition cursor-pointer">
+            <button
+              onClick={() => setOpenDropDownMenu(prev => !prev)}
+              className="h-8 w-8 grid place-items-center rounded-full hover:bg-gray-100 transition cursor-pointer"
+            >
+              <LuMenu size={18} />
+            </button>
+
+            <div className="relative w-8 h-8 rounded-full overflow-hidden">
+              <Image
+                src={'/image.png'}
+                alt='airbnb-clone-user-avatar'
+                fill
+                className='object-cover'
+              />
+            </div>
+          </div>
+
+          {/* dropdown menu */}
+          {
+            openDropDownMenu
+            &&
+            (
+              <div className="absolute right-0 top-14 w-56 bg-white border border-gray-200 rounded-xl shadow-lg overflow-hidden px-4 py-2">
+                <ul className="text-gray-800 text-sm">
+                  <li className="px-4 py-3 hover:bg-gray-100 cursor-pointer">
+                    Airbnb your home
+                  </li>
+                  <li className="px-4 py-3 hover:bg-gray-100 cursor-pointer">
+                    Help center
+                  </li>
+                  <div className="border-t my-1 border-gray-300" />
+                  <li
+                    onClick={() => openRegister()}
+                    className="px-4 py-3 hover:bg-gray-100 cursor-pointer">
+                    Sign up
+                  </li>
+                  <li
+                    onClick={() => openLogin()}
+                    className="px-4 py-3 hover:bg-gray-100 cursor-pointer">
+                    Sign in
+                  </li>
+                </ul>
+              </div>
+            )
+          }
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
