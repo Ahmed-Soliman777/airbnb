b4e8bf2b1808ecaa86fdee2a34ab57fafb9e3e31
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-05-01 20:45:31 +0300

    Create useFavorite custom hook

---
+++ hooks/useFavorites.ts
@@ -0,0 +1,37 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+export interface useFavoriteProps {
+  listingId: string;
+  currentUser?: {
+    id: string;
+    favoriteIds: string[];
+  } | null;
+}
+
+export function useFavorite({ currentUser, listingId }: useFavoriteProps) {
+  const hasFavorited = currentUser?.favoriteIds.includes(listingId);
+
+  const route = useRouter();
+
+  const toggleFavorite = async () => {
+    if (!currentUser) {
+      toast.error("Please login to favorite listings");
+      return;
+    }
+    try {
+      if (hasFavorited) {
+        await axios.delete(`/api/favorites/${listingId}`);
+      } else {
+        await axios.post(`/api/favorites/${listingId}`);
+      }
+      route.refresh();
+    } catch (error) {
+      toast.error("Something went wrong!");
+    }
+  };
+
+  return { hasFavorited, toggleFavorite };
+}
